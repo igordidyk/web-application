eb65c8ea18c39c1a5977a12d324c2f51a1abad3b
Ihor Didyk <ihordidyk95@gmail.com>
2018-04-03 20:20:18 +0300

    task_1: added authorEditing page for each authors

--- src/main/java/com/project/test/controller/AuthorsController.java
+++ src/main/java/com/project/test/controller/AuthorsController.java
@@ -2,13 +2,11 @@ package com.project.test.controller;
 
 import com.project.test.entity.Authors;
 import com.project.test.service.AuthorsService;
-import com.project.test.service.BooksService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -28,6 +26,12 @@ public class AuthorsController {
         author.setBorn(bornDate);
         authorsService.save(author);
         return "redirect:/authors";
+    }
+
+    @GetMapping("/edit-{id}")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("authorsInfo", authorsService.findOne(id));
+        return "authors/authorEditing";
     }
 
 
---
+++ src/main/webapp/pages/authors/authorEditing.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<jsp:include page="../templates/header.jsp"/>
+
+<div class="container">
+    <ol class="breadcrumb">
+        <li><a href="/">Home</a></li>
+        <li><a href="/authors">Authors</a></li>
+        <li class="active">Editing authors</li>
+    </ol>
+    <br>
+    <br>
+
+
+</div>
+
+<jsp:include page="../templates/footer.jsp"/>
