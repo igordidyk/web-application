d834254e829a5d5b52726f80cae6fc7789f4dc87
Ihor Didyk <ihordidyk95@gmail.com>
2018-04-03 18:48:19 +0300

    task_1: added controller layer for this project and mapping to index page

---
+++ src/main/java/com/project/test/controller/DefaultController.java
@@ -0,0 +1,14 @@
+package com.project.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+}
