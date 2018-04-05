82a13666df35c8d00a79627f1c70a7bf71329eb8
Ihor Didyk <ihordidyk95@gmail.com>
2018-04-05 21:56:26 +0300

    dev: completed task#2(Show authors which are older 55 years old and sort them by  column)

--- src/main/java/com/project/test/controller/DefaultController.java
+++ src/main/java/com/project/test/controller/DefaultController.java
@@ -1,11 +1,16 @@
 package com.project.test.controller;
 
+import com.project.test.entity.Authors;
 import com.project.test.service.AuthorsService;
 import com.project.test.service.BooksService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 @Controller
 public class DefaultController {
@@ -31,4 +36,25 @@ public class DefaultController {
         model.addAttribute("booksList", booksService.findAll());
         return "books/booksPage";
     }
+
+    @GetMapping("/showAuthors")
+    public String showAuthors(Model model) {
+        final List<Authors> showAuthors = new ArrayList<>();
+        List<Authors> authorsList = authorsService.findAll();
+        for (Authors author : authorsList) {
+            int year = Integer.parseInt(author.getBorn().substring(0, 4));
+            int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+            int age = currentYear - year;
+            if (age >= 55) {
+                System.out.println(author);
+                showAuthors.add(author);
+                System.out.println(showAuthors);
+            }
+            System.out.println(age);
+        }
+        model.addAttribute("showAuthors", showAuthors);
+        return "index";
+    }
+
+
 }
--- src/main/webapp/pages/index.jsp
+++ src/main/webapp/pages/index.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <jsp:include page="templates/header.jsp"/>
 
 <div class="container">
@@ -7,6 +8,36 @@
     <br>
     <p class="lead">#TASK_1: Basic CRUD for tables: <a href="/books">Books</a> and <a href="/authors">Authors</a>
     </p>
-</div>
 
+    <br>
+    <p class="lead">#TASK_2: <a href="/showAuthors">Show authors</a> which are older 55 years old and sort them by
+        `born` column</p>
+    <div id="showList">
+        <table id="showAuthors">
+            <thead>
+            <tr>
+                <th class="disable-sorting">#</th>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Born</th>
+            </tr>
+            </thead>
+            <tbody>
+            <c:forEach items="${showAuthors}" var="author">
+                <tr>
+                    <td>${author.id}</td>
+                    <td>${author.name}</td>
+                    <td>${author.gender}</td>
+                    <td>${author.born}</td>
+                </tr>
+            </c:forEach>
+            </tbody>
+        </table>
+    </div>
+
+</div>
+    <script type="text/javascript" charset="utf-8">
+        $('#showAuthors').dataTable();
+        $('#bookList').dataTable();
+    </script>
 <jsp:include page="templates/footer.jsp"/>
